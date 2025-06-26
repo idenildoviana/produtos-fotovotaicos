c9c08131d2626443ddbbb50f43f7405a7e350e59
Idenildo Viana <ideviana01@gmail.com>
2025-06-25 21:46:21 -0300

    .

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,11 +3,9 @@
 import useLocalStorage from '@/hooks/useLocalStorage';
 import { PRODUCT_CATEGORIES } from '@/utils/constants';
 import { Product } from '@/types/product';
-// Esses três estão em common
 import FormProduto from '@/components/FormProduto'
 import ListaProdutos from '@/components/ListaProdutos'
-import Header from '@/components/Header';
-import Footer from '@/components/Footer';
+
 
 
 
@@ -29,7 +27,7 @@ export default function Home() {
   return (
     <div> 
       <div>
-        <Header />
+   
         </div>
 
       <div className='container mx-auto md:flex justify-center  p-8 md:p-10'  >
@@ -40,7 +38,7 @@ export default function Home() {
         categories={PRODUCT_CATEGORIES}
       />
       </div>
-<Footer/>
+
     </div>
   );
 }
